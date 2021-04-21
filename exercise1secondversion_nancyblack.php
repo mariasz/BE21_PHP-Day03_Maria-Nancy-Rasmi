0ecefe02755a2849c2aa61c0755896258d2ec999
ncyblk <82343905+ncyblk@users.noreply.github.com>
2021-04-21 16:03:20 +0200

    Add files via upload

---
+++ exercise1secondversion_nancyblack.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" >
+   <head>
+       <meta charset="UTF-8">
+        <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+       <title ></title>
+   </head>
+   <body>
+       <?php
+       $myname = "Nancy Black";
+       $a = 0;
+        while ($a<50) {
+            $a++;
+            echo $a.":".$myname."</br>";
+        }
+
+             ?>
+    </body>
+    </ html>
