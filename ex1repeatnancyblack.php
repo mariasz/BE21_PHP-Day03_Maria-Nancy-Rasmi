2bfdb608843d135a5eb42af1040c620fdc550a7e
ncyblk <82343905+ncyblk@users.noreply.github.com>
2021-04-21 15:54:50 +0200

    Add files via upload

---
+++ ex1repeatnancyblack.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" >
+   <head>
+       <meta charset="UTF-8">
+        <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+       <title ></title>
+   </head>
+   <body>
+       <?php
+       $myname = "Nancy Black";;
+       for ( $a= "0";$a <= 50;$a++) {
+           echo $a .$myname. "</br>";
+       }
+
+             ?>
+    </body>
+    </ html>
---
+++ ex2repeat_nancyblack.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" >
+   <head>
+       <meta charset="UTF-8">
+        <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+       <title ></title>
+   </head>
+   <body>
+       <?php
+       $cookies = ["choc", "hazel", "vanilla",
+       "strawberry", "rasberry", "oreo",
+       "nosugar", "nuts", "twix", "mars"];
+       foreach ($cookies as $value) {
+           echo $value. "</br>";
+              }
+
+       
+
+       ?>
+   </body>
+</ html>
